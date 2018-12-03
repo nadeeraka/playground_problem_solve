4ecd73005fa78af0a3f425f2089b549227c81cf6
nadeeraka <nimanthanadeeraka@outlook.com>
2018-12-03 14:36:52 +0530

    trim 2

--- projects/reverse.js
+++ projects/reverse.js
@@ -69,24 +69,81 @@
 // console.log(v);
 
 // using .toString | math.sign |   parseInt
-let worad = -51;
-let hold = " ";
+// let worad = 4368888;
+// let hold = " ";
 
-let reverse = w => {
-  let value = w;
-  if (Math.sign(value) === 1) {
-    value === NaN ? value : (value += "");
-    for (const i of value) {
-      hold = i + hold;
-    }
-  } else {
-    value === NaN ? value : (value += "");
-    let shift = value.split("");
-    let reverse = shift.reverse().join("");
-    let int = parseInt(reverse);
-    return (neg = int * -1);
-  }
+// let reverse = w => {
+//   let value = w;
+//   if (Math.sign(value) === 1) {
+//     value === NaN ? value : (value += "");
+//     for (const i of value) {
+//       hold = i + hold;
+//     }
+//     let int = parseInt(hold);
+//     return int;
+//   } else {
+//     value === NaN ? value : (value += "");
+//     let shift = value.split("");
+//     let reverse = shift.reverse().join("");
+//     let int = parseInt(reverse);
+//     return (neg = int * -1);
+//   }
+// };
+// // let v = reverse(worad) === NaN ? " worng" : "correct";
+// // console.log(v);
+// console.log(reverse(worad));
+//------------------------------------------------------------------
+//short answer
+// const number = -67;
+
+// const convert = number => {
+//   if (Math.sign(number) === -1) {
+//     const array = number
+//       .toString()
+//       .split("")
+//       .reverse("")
+//       .join("");
+//     const n = parseInt(array);
+//     return n * -1;
+//   }
+//   const array = number
+//     .toString()
+//     .split("")
+//     .reverse("")
+//     .join("");
+//   const n = parseInt(array);
+//   return n;
+// };
+// console.log(convert(number));
+
+//---------------------------------------------------------------------
+//improve answer
+
+// const number = 23;
+
+// const convert = number => {
+//   const array = number
+//     .toString()
+//     .split("")
+//     .reverse("")
+//     .join("");
+//   let n = parseInt(array);
+
+//   Math.sign(number) === -1 ? (n *= -1) : n;
+//   return n;
+// };
+// console.log(convert(number));
+//--------------------------------------------------------
+//more improve
+const number = 23;
+
+const convert = number => {
+  const array = number
+    .toString()
+    .split("")
+    .reverse("")
+    .join("");
+  let n = parseInt(array);
+  return n * Math.sign(number);
 };
-// let v = reverse(worad) === NaN ? " worng" : "correct";
-// console.log(v);
-console.log(reverse(worad));
+console.log(convert(number));
